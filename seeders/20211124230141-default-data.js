a73fe6592a2955e3512c1c1591770b8a09308540
D3NCyan <edwarddoltons@gmail.com>
2021-11-25 08:24:48 +0800

    create seed

---
+++ seeders/20211124230141-default-data.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config()
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const hashedPassword = await bcrypt.hash('Admin1234', 10)
+    
+    await queryInterface.bulkInsert("users", [
+      {
+        fullName: "Doltons Edward Nic",
+        email: "admin@literature.com",
+        password: hashedPassword,
+        gender: "Male",
+        phone: "089619800459",
+        address: process.env.PATH_AVATAR_DEFAULT,
+        role: "admin"
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
