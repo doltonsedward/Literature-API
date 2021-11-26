8355613ff70be247801fa0a0572f8088a404dd9d
D3NCyan <edwarddoltons@gmail.com>
2021-11-27 06:58:55 +0800

    done profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const { register, login, checkAuth } = require('../controllers/auth')
 const { auth } = require('../middlewares/auth')
-const { getLiterature, addLiterature, deleteLiterature, searchLiterature } = require('../controllers/literature')
+const { getLiterature, addLiterature, deleteLiterature, searchLiterature, detailLiterature } = require('../controllers/literature')
 const { uploadFile } = require('../middlewares/uploadFile')
 const { updateUser } = require('../controllers/user')
 const { uploadImage } = require('../middlewares/uploadImage')
-const { getCollection } = require('../controllers/collection')
+const { getCollection, addCollection } = require('../controllers/collection')
+const { getOwnerLiterature } = require('../controllers/profile')
 
 const router = express.Router()
 
+// user or profile
 router.patch('/user', auth, uploadImage("avatar", "uploads/avatar-external"), updateUser)
+router.get('/profile/:profile_id/literature', getOwnerLiterature)
 
 // literature session
 router.get('/literatures', getLiterature)
+router.get('/literature/:literature_id', detailLiterature)
 router.get('/search-literatures', searchLiterature)
 router.post('/literature', auth, uploadFile("attache", "uploads/literatures"), addLiterature)
 router.delete('/literature/:id', auth, deleteLiterature)
 
 // collection session
 router.get('/collection/:profile_id', auth, getCollection)
+router.post('/collection/:literature_id', auth, addCollection)
 
 // login regis session
 router.post('/register', register)
