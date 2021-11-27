6b73ba12c39c139030cc0baac7b80ba2e3ae611c
D3NCyan <edwarddoltons@gmail.com>
2021-11-27 23:23:11 +0800

    add status in literature

--- migrations/20211124003858-create-literature.js
+++ migrations/20211124003858-create-literature.js
@@ -35,6 +35,9 @@ module.exports = {
         onUpdate: "CASCADE",
         onDelete: "CASCADE"
       },
+      status: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
