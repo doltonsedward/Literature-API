b9a257563bff62155af990a25a2aa2b2e7a33ed2
D3NCyan <edwarddoltons@gmail.com>
2021-11-29 14:33:06 +0800

    add getCollection by literature in controllers

--- src/controllers/collection.js
+++ src/controllers/collection.js
@@ -2,7 +2,7 @@ const { collection, literature, user } = require('../../models')
 
 exports.getCollection = async (req, res) => {
     try {
-        const { profile_id } = req.params
+        const { id } = req.user
 
         const response = await collection.findAll({
             include: [
@@ -17,7 +17,7 @@ exports.getCollection = async (req, res) => {
                     model: user,
                     as: "ownerCollection",
                     where: {
-                        id: profile_id
+                        id
                     }
                 },
             ],
@@ -44,23 +44,91 @@ exports.getCollection = async (req, res) => {
     }
 }
 
+exports.getCollectionByLiterature = async (req, res) => {
+    try {
+        const { id } = req.user
+        const { literature_id } = req.params
+
+        const response = await collection.findOne({
+            include: [
+                {
+                    model: literature,
+                    as: "literature",
+                    attributes: {
+                        exclude: ["updatedAt", "createdAt"]
+                    },
+                    where: {
+                        id: literature_id
+                    }
+                },
+                {
+                    model: user,
+                    as: "ownerCollection",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    },
+                    where: {
+                        id
+                    }
+                }
+            ]
+        })
+
+        res.send({
+            status: "success",
+            literature: response
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            status: "failed",
+            message: "Internal server error"
+        })
+    }
+}
+
 exports.addCollection = async (req, res) => {
     try {
         const { id } = req.user
         const { name } = req.body
         const { literature_id } = req.params
         
-        await collection.create({
-            name: name ?? 'My Collection',
+        const response = await collection.create({
+            name: name ? name : 'My Collection',
             userId: id,
             literatureId: literature_id
         })
+
+        console.log(response)
 
         res.send({
             status: "success",
             message: "Add collection finished"
         })
     } catch (error) {
+        res.status(500).send({
+            status: "failed",
+            message: "Internal server error"
+        })
+    }
+}
+
+exports.deleteCollection = async (req, res) => {
+    try {
+        const { collection_id } = req.params
+        
+        await collection.destroy({
+            where: {
+                id: collection_id
+            }
+        })
+
+        res.send({
+            status: "success",
+            message: `Delete collection id: ${collection_id} finished`
+        })
+    } catch (error) {
+        console.log(error)
         res.status(500).send({
             status: "failed",
             message: "Internal server error"
