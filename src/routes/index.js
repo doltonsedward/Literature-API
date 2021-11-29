ec904a7b9a8474c24288862c0ca36f149cf2d805
D3NCyan <edwarddoltons@gmail.com>
2021-11-29 14:36:12 +0800

    fix routes and add oauth logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express')
-const { register, login, checkAuth } = require('../controllers/auth')
+const { register, login, checkAuth, loginOauth } = require('../controllers/auth')
 const { auth } = require('../middlewares/auth')
 const { getLiterature, addLiterature, deleteLiterature, searchLiterature, detailLiterature, updateLiterature } = require('../controllers/literature')
 const { uploadFile } = require('../middlewares/uploadFile')
-const { updateUser } = require('../controllers/user')
+const { updateUser, updateUserSpecificData } = require('../controllers/user')
 const { uploadImage } = require('../middlewares/uploadImage')
-const { getCollection, addCollection } = require('../controllers/collection')
+const { getCollection, addCollection, getCollectionByLiterature, deleteCollection } = require('../controllers/collection')
 const { getOwnerLiterature } = require('../controllers/profile')
+const { oauthGoogle } = require('../controllers/oauth')
 
 const router = express.Router()
 
 // user or profile
 router.patch('/user', auth, uploadImage("avatar", "uploads/avatar-external"), updateUser)
+router.patch('/user/specific', auth, updateUserSpecificData)
 router.get('/profile/:profile_id/literature', getOwnerLiterature)
 
 // literature session
@@ -23,12 +25,16 @@ router.patch('/literature/:literature_id', auth, updateLiterature)
 router.delete('/literature/:literature_id', auth, deleteLiterature)
 
 // collection session
-router.get('/collection/:profile_id', auth, getCollection)
+router.get('/collection', auth, getCollection)
+router.get('/collection/:literature_id', auth, getCollectionByLiterature)
 router.post('/collection/:literature_id', auth, addCollection)
+router.delete('/collection/:collection_id', auth, deleteCollection)
 
 // login regis session
 router.post('/register', register)
 router.post('/login', login)
+
+router.post('/auth/google', oauthGoogle)
 
 router.get('/check-auth', auth, checkAuth)
 
